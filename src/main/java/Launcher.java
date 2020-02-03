4e7440890a24ce282466adf8b0514166407a5c6d
iason <mvm283@gmail.com>
2020-02-03 03:21:04 +0100

    edit command line and add jar file

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -14,12 +14,12 @@ public class Launcher {
     public static void main( String[] args ) throws Exception {
 
         //run from os comand Terminal
-        //UserInterface.entryPoint(args);
+        UserInterface.entryPoint(args);
 
         //test run from Intelij
         //UserInterface.runFromIntelij();
 
-        produceFromStringTest();
+        //produceFromStringTest();
 
     }
 
